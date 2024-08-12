94f467ad0f0788bcaa5dd657d7e92fea1206844d
Mohammed Rokon Uddin <shoaib.rokon@gmail.com>
2024-08-12 14:15:16 +0600

    add solution for remove-element

---
+++ SwiftCode.playground/Sources/Array/RemoveElement.swift
@@ -0,0 +1,12 @@
+/// Question Link: https://leetcode.com/problems/remove-element/
+/// Primary idea: keep a index, compare the element at that index with val while moving forward
+/// Time Complexity: O(n), Space Complexity: O(1)
+///
+
+public struct RemoveElement {
+  public func removeElement(_ nums: inout [Int], _ val: Int) -> Int {
+    nums = nums.filter{ $0 != val }
+    return nums.count
+  }
+}
+
